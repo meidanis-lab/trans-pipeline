98c15bcefeccc1d6b1d69dd1da137c241e7f09f4
Ana Luiza <analuseidinger@gmail.com>
2024-11-01 15:22:23 +0300

    corrected target names and changed TABS to four spaces

--- pre-proc.sh
+++ pre-proc.sh
@@ -6,19 +6,19 @@ echo $SAMPLE
 
 #005_preqc
 for s in $SAMPLE; do
-	make -C 005_preqc ${s}_1_fastqc.html
-	make -C 005_preqc ${s}_2_fastqc.html
+    make -C 005_preqc ${s}_1_fastqc.html
+    make -C 005_preqc ${s}_2_fastqc.html
 done
 
 #010_trim
 for s in $SAMPLE; do
-	make -C 010_trim ${s}_1.paired.fastq.gz
+    make -C 010_trim ${s}_1.paired.fastq.gz
 done
 
 #015_posqc
 for s in $SAMPLE; do
-	make -C 015_posqc ${s}_1.paired.fastq.html
-	make -C 015_posqc ${s}_2.paired.fastq.html
+    make -C 015_posqc ${s}_1.paired_fastqc.html
+    make -C 015_posqc ${s}_2.paired_fastqc.html
 done
 
 #020_map
@@ -28,7 +28,7 @@ done
 
 #025_sam
 for s in $SAMPLE; do
-	make -C 025_sam ${s}.paired.bam
+    make -C 025_sam ${s}.paired.bam
 done
 
 ##Deleting .sam files
@@ -36,7 +36,7 @@ done
 
 #030_assemble
 for s in $SAMPLE; do
-	make -C 030_assemble ${s}.paired.gtf
+    make -C 030_assemble ${s}.paired.gtf
 done
 
 #035_merge
@@ -44,7 +44,7 @@ make -C 035_merge merge_list.gtf
 
 #040_estimations
 for s in $SAMPLE; do
-	make -C 040_estimations ${s}.paired.gtf
+    make -C 040_estimations ${s}.paired.gtf
 done
 
 #045_prepde
